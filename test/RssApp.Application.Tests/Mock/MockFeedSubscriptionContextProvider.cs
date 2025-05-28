d0f819c2db086876062630e73b870d9dc44f686f
Josh Brunton <josh.brunton@binary.ax>
2025-05-28 11:25:58 +0100

    test: Add CRUD tests for feed subscriptions

---
+++ test/RssApp.Application.Tests/Mock/MockFeedSubscriptionContextProvider.cs
@@ -0,0 +1,16 @@
+﻿using RssApp.Application.Services.ContextProviders.FeedSubscriptions;
+using RssApp.DataAccess.Context;
+
+namespace RssApp.Application.Tests.Mock;
+
+internal class MockFeedSubscriptionContextProvider : IFeedSubscriptionContextProvider
+{
+    public string FileName { get; } = $"Data Source=test-{Guid.NewGuid()}.db";
+
+    public IFeedSubscriptionsContext New()
+    {
+        var ret = new FeedSubscriptionsContext(FileName);
+        ret.Database.EnsureCreated();
+        return ret;
+    }
+}
---
+++ test/RssApp.Application.Tests/ViewModels/FeedSubscription/FeedSubscriptionEditViewModelTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RssApp.Application.Extensions.System.Windows.Input;
+using RssApp.Application.Models.FeedSubscription;
+using RssApp.Application.Services.ContextProviders.FeedSubscriptions;
+using RssApp.Application.Tests.Mock;
+using RssApp.Application.ViewModels.FeedSubscription;
+
+namespace RssApp.Application.Tests.ViewModels.FeedSubscription;
+
+[TestClass]
+public class FeedSubscriptionEditViewModelTests
+{
+    [TestMethod]
+    public async Task Entity_ShouldBeChanged_AfterEdit()
+    {
+        var serviceBuilder = new ServiceCollection();
+        serviceBuilder.AddSingleton<IFeedSubscriptionContextProvider, MockFeedSubscriptionContextProvider>();
+        IServiceProvider services = serviceBuilder.BuildServiceProvider();
+
+        const string testFeedName = "Test feed";
+        const string testFeedUri = "https://example.com/feed.xml";
+        var feedSubscription = new Core.FeedSubscription { FeedName = testFeedName, FeedUri = testFeedUri };
+        await using (var ctx = services.GetRequiredService<IFeedSubscriptionContextProvider>().New())
+        {
+            await ctx.FeedSubscriptions.AddAsync(feedSubscription);
+            await ctx.SaveChangesAsync();
+        }
+
+        await using (var ctx = services.GetRequiredService<IFeedSubscriptionContextProvider>().New())
+        {
+            bool exists = await ctx.FeedSubscriptions.AnyAsync(x => x.FeedSubscriptionId == feedSubscription.FeedSubscriptionId);
+            Assert.IsTrue(exists);
+        }
+
+        const string newFeedName = "Feed name has changed";
+        var viewModel = new FeedSubscriptionEditViewModel(services);
+        await viewModel.InitAsync(new FeedSubscriptionModel(feedSubscription));
+        viewModel.FeedName = newFeedName;
+        viewModel.SaveCommand.Execute();
+
+        await using (var ctx = services.GetRequiredService<IFeedSubscriptionContextProvider>().New())
+        {
+            var record = await ctx.FeedSubscriptions.SingleOrDefaultAsync(x => x.FeedSubscriptionId == feedSubscription.FeedSubscriptionId);
+            ArgumentNullException.ThrowIfNull(record);
+            Assert.IsTrue(record.FeedName == newFeedName);
+        }
+    }
+}
