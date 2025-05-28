44623878087ec3683605337160275ceed195231e
Josh Brunton <josh.brunton@binary.ax>
2025-05-28 11:25:02 +0100

    feat(app): Add feed subscription edit view model
    Added a viewmodel to edit/update a subscription.

---
+++ src/RssApp.Application/ViewModels/FeedSubscription/FeedSubscriptionEditViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using RssApp.Application.Models.FeedSubscription;
+using RssApp.Application.Services.ContextProviders.FeedSubscriptions;
+
+namespace RssApp.Application.ViewModels.FeedSubscription;
+
+public partial class FeedSubscriptionEditViewModel : ObservableObject
+{
+    private readonly IFeedSubscriptionContextProvider _dbContextProvider;
+
+    private Guid _feedSubscriptionId;
+
+    public ICommand SaveCommand { get; }
+
+    [ObservableProperty] public partial string FeedName { get; set; } = "";
+    [ObservableProperty] public partial string FeedUri { get; set; } = "";
+
+    public FeedSubscriptionEditViewModel(IServiceProvider services)
+    {
+        _dbContextProvider = services.GetRequiredService<IFeedSubscriptionContextProvider>();
+        SaveCommand = new RelayCommand(Save);
+    }
+
+    public async Task InitAsync(FeedSubscriptionModel feedSubscriptionModel)
+    {
+        _feedSubscriptionId = feedSubscriptionModel.ToPoco().FeedSubscriptionId;
+
+        await using var ctx = _dbContextProvider.New();
+        var record = await ctx.FeedSubscriptions.SingleOrDefaultAsync(x => x.FeedSubscriptionId == _feedSubscriptionId);
+
+        if (record is null)
+        {
+            throw new Exception($"Could not find a {nameof(Core.FeedSubscription)} record " +
+                                $"with {nameof(Core.FeedSubscription.FeedSubscriptionId)} {_feedSubscriptionId}.");
+        }
+
+        FeedName = record.FeedName;
+        FeedUri = record.FeedUri;
+    }
+
+    private async void Save()
+    {
+        await using var ctx = _dbContextProvider.New();
+        var record = await ctx.FeedSubscriptions.SingleOrDefaultAsync(x => x.FeedSubscriptionId == _feedSubscriptionId);
+
+        if (record is null)
+        {
+            throw new Exception($"Could not find a {nameof(Core.FeedSubscription)} record " +
+                                $"with {nameof(Core.FeedSubscription.FeedSubscriptionId)} {_feedSubscriptionId}.");
+        }
+
+        record.FeedName = FeedName;
+        record.FeedUri = FeedUri;
+
+        await ctx.SaveChangesAsync();
+    }
+}
