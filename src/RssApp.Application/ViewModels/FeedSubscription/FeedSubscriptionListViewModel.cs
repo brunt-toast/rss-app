c6a035df3deaaf2284a5288c8456726560853c6f
Josh Brunton <josh.brunton@binary.ax>
2025-05-28 11:24:29 +0100

    feat(app): Add feed subscription list view model
    Created a viewmodel to list feed subscriptions.

---
+++ src/RssApp.Application/ViewModels/FeedSubscription/FeedSubscriptionListViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.Extensions.DependencyInjection;
+using RssApp.Application.Extensions.System.Collections.ObjectModel;
+using RssApp.Application.Models.FeedSubscription;
+using RssApp.Application.Services.ContextProviders.FeedSubscriptions;
+
+namespace RssApp.Application.ViewModels.FeedSubscription;
+
+public partial class FeedSubscriptionListViewModel : ObservableObject
+{
+    private readonly IFeedSubscriptionContextProvider _dbContextProvider;
+
+    public ObservableCollection<FeedSubscriptionModel> Feeds { get; } = [];
+
+    public FeedSubscriptionListViewModel(IServiceProvider services)
+    {
+        _dbContextProvider = services.GetRequiredService<IFeedSubscriptionContextProvider>();
+    }
+
+    public async Task InitAsync()
+    {
+        await using var ctx = _dbContextProvider.New();
+
+        IEnumerable<Core.FeedSubscription> feedSubscriptions = ctx.FeedSubscriptions;
+        IEnumerable<FeedSubscriptionModel> feedSubscriptionModels = feedSubscriptions.Select(x => new FeedSubscriptionModel(x));
+
+        await Feeds.ReplaceRangeAsync(feedSubscriptionModels);
+    }
+}
