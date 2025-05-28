4a135e894e3224dde408d6a964163bd356475af4
Josh Brunton <josh.brunton@binary.ax>
2025-05-28 11:09:39 +0100

    feat(core): Add FeedSubscription POCO
    Added the FeedSubscription POCO to the new core project.

---
+++ src/RssApp.Core/FeedSubscription.cs
@@ -0,0 +1,8 @@
+﻿namespace RssApp.Core;
+
+public class FeedSubscription
+{
+    public Guid FeedSubscriptionId { get; set; } = Guid.NewGuid();
+    public string FeedName { get; set; } = "";
+    public string FeedUri { get; set; } = "";
+}
