f7be107c199c79ef94f4cc4fc139214f74da0a1d
Josh Brunton <josh.brunton@binary.ax>
2025-05-30 15:30:39 +0100

    feat(core): Add FeedFilter and junction
    Added a FeedFilter entity and a junction to connect it to
    FeedSubscription.

---
+++ src/RssApp.Core/Enums/Flags/FeedFilterAppliesToFlags.cs
@@ -0,0 +1,9 @@
+﻿namespace RssApp.Core.Enums.Flags;
+
+[Flags]
+public enum FeedFilterAppliesToFlags
+{
+    Uri = 0x0001,
+    Title = 0x0002,
+    Body = 0x0004
+}
---
+++ src/RssApp.Core/Junctions/FeedSubscriptionFeedFilter.cs
@@ -0,0 +1,7 @@
+﻿namespace RssApp.Core.Junctions;
+
+public class FeedSubscriptionFeedFilter
+{
+    public Guid FeedSubscriptionId { get; set; }
+    public Guid FeedFilterId { get; set; }
+}
