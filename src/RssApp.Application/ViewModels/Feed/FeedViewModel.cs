b25914d9e80b447f49c87dadf2294007df3eb4d9
Josh Brunton <josh.brunton@proton.me>
2025-06-01 13:00:29 +0100

    feat: Add feed viewmodel

---
+++ src/RssApp.Application/ViewModels/Feed/FeedViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+using CodeHollow.FeedReader;
+using CommunityToolkit.Mvvm.ComponentModel;
+using RssApp.Application.Extensions.System.Collections.ObjectModel;
+using RssApp.Application.Models.FeedSubscription;
+
+namespace RssApp.Application.ViewModels.Feed;
+
+public partial class FeedViewModel : ObservableObject
+{
+    ObservableCollection<FeedItem> Items { get; } = [];
+
+    public async Task InitAsync(FeedSubscriptionModel model)
+    {
+        var feed = await FeedReader.ReadAsync(model.FeedUri);
+        await Items.ReplaceRangeAsync(feed.Items);
+    }
+}
