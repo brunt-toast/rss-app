532290b2759c3d8051f9633fd5cb5ec3fe805516
Josh Brunton <josh.brunton@binary.ax>
2025-06-02 09:24:02 +0100

    test: Add MockDialogService

---
+++ test/RssApp.Application.Tests/Mock/MockDialogService.cs
@@ -0,0 +1,15 @@
+﻿using RssApp.Application.Services.Dialogs;
+
+namespace RssApp.Application.Tests.Mock;
+
+internal class MockDialogService : IDialogService
+{
+    public event EventHandler<ConfirmationRequestedEventArgs>? ConfirmationRequested;
+
+    public async Task<bool> RequestConfirmationAsync(string title, string message)
+    {
+        var args = new ConfirmationRequestedEventArgs(title, message, "Confirm", "Cancel");
+        ConfirmationRequested?.Invoke(this, args);
+        return await args.ConfirmationSource.Task;
+    }
+}
